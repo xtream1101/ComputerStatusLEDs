4971b2fb213fddfd290e535923b3ce41e94795db
Eddy Hintze <eddy.hintze@gmail.com>
2014-06-30 13:25:05 -0400

    Added model number feedback

--- arduino/ComputerStatusLEDs/ComputerStatusLEDs.ino
+++ arduino/ComputerStatusLEDs/ComputerStatusLEDs.ino
@@ -2,6 +2,8 @@ int latchPin = 5;
 int clockPin = 6;
 int dataPin = 4;
 
+char* MODEL = "XN-LED2-5C95";
+
 String cmd;
 char colorList[] = {'R', 'G', 'B'};
 int numLeds = 2;
@@ -41,23 +43,27 @@ void loop(){
   }
 
   if(cmd.length() > 0){
-    for(int i=1; i<=numLeds; i++){
-      int stateI = i*3;
-      if(cmd[0]-'0' == i){
-        for(int j=0; j<sizeof(colorList); j++){
-          int stateJ = j-3;
-          int stateIJ = stateI + stateJ;
-          if(cmd[1] == colorList[j]){
-            if(cmd[2] == '1')      ledState[stateIJ] = true;
-            else if(cmd[2] == 'T') ledState[stateIJ] = !ledState[stateIJ];
-            else                   ledState[stateIJ] = false;
-          }
-        } //END for(j)
-      }
-    } //END for(i)
+    if(cmd[0] == '?'){
+      Serial.write(MODEL);
+    }else{
+      for(int i=1; i<=numLeds; i++){
+        int stateI = i*3;
+        if(cmd[0]-'0' == i){
+          for(int j=0; j<sizeof(colorList); j++){
+            int stateJ = j-3;
+            int stateIJ = stateI + stateJ;
+            if(cmd[1] == colorList[j]){
+              if(cmd[2] == '1')      ledState[stateIJ] = true;
+              else if(cmd[2] == 'T') ledState[stateIJ] = !ledState[stateIJ];
+              else                   ledState[stateIJ] = false;
+            }
+          } //END for(j)
+        }
+      } //END for(i)
+      setLeds();
+    } //END if(?)
     cmd="";
-    setLeds();
-  } 
+  } //END if(cmd.length) 
 } //END loop()
 
 void setLeds(){
